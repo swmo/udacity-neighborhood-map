e91c55177fc5c32d223fe0d3bd44f780452ff010
Moses Tschanz <moses.tschanz@gmail.com>
2019-01-23 22:43:16 +0100

    prepare maps

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function initMap() {
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7413549, lng: -73.9980244},
+      zoom: 13,
+      mapTypeControl: false
+    });
+
+}
